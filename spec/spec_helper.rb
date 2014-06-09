7e444b788af6f92ef1e7bb4392d846b720997b10
DanBradbury <dan.luckydaisy@gmail.com>
2014-06-09 00:34:32 -0700

    Reset after test timer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,5 @@ RSpec.configure do |config|
   config.color_enabled = true
   config.order = 'random'
   #XXX: need better sleep pattern
-  #config.after(:all) {sleep 10} #allows us to run entire test suite without breaking request limit
+  config.after(:all) {sleep 10} #allows us to run entire test suite without breaking request limit
 end
