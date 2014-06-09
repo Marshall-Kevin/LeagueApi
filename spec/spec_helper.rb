ebef5059f2bf0d2c40a533ff6a7482c3128b5135
DanBradbury <dan.luckydaisy@gmail.com>
2014-06-09 00:41:54 -0700

    Remove rspec color deprecation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'rspec'
 require 'league_api'
 
 RSpec.configure do |config|
-  config.color_enabled = true
   config.order = 'random'
   #XXX: need better sleep pattern
   config.after(:all) {sleep 10} #allows us to run entire test suite without breaking request limit
